759af037117ad443949c10d2bfc7009963c2dcb6
Anton Nesterov <antonnesterov939@gmail.com>
2020-07-11 13:27:01 +0300

    adding anton_2

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EnterPassword from "../views/EnterPassword";
+import PasswordChangedPopUp from "../views/PasswordChangedPopUp";
 // import Recovery from '../views/Recovery'
 
-Vue.use(VueRouter)
-const routes=[{
-    path:'/recovery',
-    name: 'EmailRecovery',
-    meta:{layout:'empty'},
-    component: () =>import('../views/EmailRecovery.vue')
-},
-{
-    path:'/menu',
-    name:'Menu',
-    meta:{layout:'main'},
-    component: () =>import('../views/Menu.vue')
-},
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: "/recovery",
+    name: "EmailRecovery",
+    meta: { layout: "empty" },
+    component: () => import("../views/EmailRecovery.vue")
+  },
+  {
+    path: "/menu",
+    name: "Menu",
+    meta: { layout: "main" },
+    component: () => import("../views/Menu.vue")
+  },
 
+  {
+    path: "/",
+    name: "Login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/enterpassword",
+    name: "EnterPassword",
+    component: EnterPassword
+  },
+  {
+    path: "/passwordchangedpopup",
+    name: "PasswordChangedPopUp",
+    component: PasswordChangedPopUp
+  }
+];
 
-{
-    path:'/',
-    name:'Login',
-    meta:{layout:'empty'},
-    component: () => import('../views/Login.vue')
-}
-]
-
-const router =new VueRouter({
-    mode:'history',
-    base: process.env.BASE_URL,
-    routes
-})
-export default router 
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+export default router;
--- src/views/EmailRecovery.vue
+++ src/views/EmailRecovery.vue
@@ -1,37 +1,40 @@
 <template>
-<b-container fluid="lg">
+  <b-container fluid="lg">
     <b-row>
       <b-col>
         <form v-if="!sent" v-on:submit.prevent="submitForm">
           <div className="form-row">
             <div className="form-field">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" v-model.trim="email"/>
+              <input type="email" id="email" v-model.trim="email" />
             </div>
           </div>
           <button type="submit" v-on:click="sendEmail">Отправить e-mail</button>
+          <router-link to="/passwordchangedpopup"
+            ><b-button>Перейти к Антону</b-button></router-link
+          >
         </form>
-        <div v-if="sent" class='success'>
-          <p>{{msg}}</p>
+        <div v-if="sent" class="success">
+          <p>{{ msg }}</p>
         </div>
-       </b-col>
+      </b-col>
     </b-row>
-</b-container>
+  </b-container>
 </template>
 
 <script>
-  export default {
-    name: 'EmailRecovery',
-    data () {
-      return {
-       sent: false,
-       msg: 'E-mail has been succesfully sent!'
-      }
-    },
-    methods:{
-      sendEmail:function(){
-        this.sent = true;
-}
-}
+export default {
+  name: "EmailRecovery",
+  data() {
+    return {
+      sent: false,
+      msg: "E-mail has been succesfully sent!"
+    };
+  },
+  methods: {
+    sendEmail: function() {
+      this.sent = true;
+    }
   }
+};
 </script>
